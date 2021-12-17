e9822978492425974db9156ef975466f1e486be2
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-17 16:52:02 -0500

    [GREEN] test_print_failure

--- xunit.py
+++ xunit.py
@@ -12,8 +12,11 @@ def assert_equal(expected: Any, actual: Any):
 
 class XUnitTestRunner:
     def run_test_function(self, test_function: Callable):
-        test_function()
-        self.print(f"[SUCCESS] {test_function.__name__}")
+        try:
+            test_function()
+            self.print(f"[SUCCESS] {test_function.__name__}")
+        except XUnitTestFailure:
+            self.print(f"[FAILURE] {test_function.__name__}")
 
     def print(self, text: str):
         print(text)
