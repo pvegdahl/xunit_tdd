4e067c96387f1eb9af1bd901fdfb1ce0a9cf1691
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-17 16:37:34 -0500

    [RED] test_print_success
    Note that to make this test work, we are building our own spy
    class to catch printing.  This does couple us slightly to an
    implementation detail.  However, it is minor, and gives us a
    relatively clean path forward.

--- test_xunit.py
+++ test_xunit.py
@@ -1,6 +1,14 @@
 from xunit import assert_equal, XUnitTestFailure, XUnitTestRunner
 
 
+class SpyTestRunner(XUnitTestRunner):
+    def __init__(self):
+        self.printed = []
+
+    def print(self, text: str):
+        self.printed.append(text)
+
+
 def test_can_run_test_function():
     was_run = False
 
@@ -30,8 +38,18 @@ def test_assert_equal_does_not_raise_exception_on_equal():
         print("FAILURE")
 
 
+def test_print_success():
+    def test_success():
+        pass
+
+    spy_test_runner = SpyTestRunner()
+    spy_test_runner.run_test_function(test_success)
+    assert_equal(["SUCCESS"], spy_test_runner.printed)
+
+
 if __name__ == "__main__":
     test_runner = XUnitTestRunner()
     test_runner.run_test_function(test_can_run_test_function)
     test_runner.run_test_function(test_assert_equal_raises_exception_on_unequal)
     test_runner.run_test_function(test_assert_equal_does_not_raise_exception_on_equal)
+    test_runner.run_test_function(test_print_success)
