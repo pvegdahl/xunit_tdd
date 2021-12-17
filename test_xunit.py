f9fad28ca3c25e327c86ef22d6b9368e39b3fd61
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-17 16:21:31 -0500

    Failing test: test_can_run_test_function

---
+++ test_xunit.py
@@ -0,0 +1,20 @@
+from xunit import run_test_function
+
+
+def test_can_run_test_function():
+    was_run = False
+
+    def test_function():
+        nonlocal was_run
+        was_run = True
+
+    run_test_function(test_function)
+
+    if was_run:
+        print("SUCCESS")
+    else:
+        print("FAILURE")
+
+
+if __name__ == "__main__":
+    test_can_run_test_function()
---
+++ xunit.py
@@ -0,0 +1,5 @@
+from typing import Callable
+
+
+def run_test_function(test_function: Callable):
+    pass
