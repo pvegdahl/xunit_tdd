e24a7136bbddf3ef601624cf5b665a76d9ba30b6
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-17 16:54:29 -0500

    [GREEN] Update test_print_failure to include failure message

--- xunit.py
+++ xunit.py
@@ -7,7 +7,7 @@ class XUnitTestFailure(Exception):
 
 def assert_equal(expected: Any, actual: Any):
     if expected != actual:
-        raise XUnitTestFailure()
+        raise XUnitTestFailure(f"Expected {expected}, got {actual}")
 
 
 class XUnitTestRunner:
@@ -15,8 +15,8 @@ class XUnitTestRunner:
         try:
             test_function()
             self.print(f"[SUCCESS] {test_function.__name__}")
-        except XUnitTestFailure:
-            self.print(f"[FAILURE] {test_function.__name__}")
+        except XUnitTestFailure as assertion_failure:
+            self.print(f"[FAILURE] {test_function.__name__}: {assertion_failure}")
 
     def print(self, text: str):
         print(text)
